80b51f652e6db3382e6651e4c1d3d9af43c9e3bb
Galitan-dev <galitan.dev@gmail.com>
2022-02-24 18:20:12 +0100

    feat(frontend): added feedback list, including search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,54 @@
+import { Component } from 'react';
 import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={process.env.PUBLIC_URL + '/icon.png'} className="App-logo" alt="Star" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          It's Working !
-        </p>
-        <a
-          className="App-link"
-          href={process.env.REACT_APP_API_URL + '/feedbacks'}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          List feedbacks
-        </a>
-      </header>
-    </div>
-  );
-}
+import Feedback from './feedback/Feedback';
 
-export default App;
+export default class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { feedbacks: [] };
+  }
+
+  async componentDidMount() {
+    const response = await fetch(process.env.REACT_APP_API_URL + '/feedbacks');
+    const data = await response.json();
+    this.setState({ feedbacks: data });
+  }
+
+  async filter(search) {
+    const response = await fetch(process.env.REACT_APP_API_URL + '/feedbacks');
+    /**  @type {{ profilePicture: string, author: string, title: string, body: string, createdAt: string, stars: number }[]} */
+    const data = await response.json();
+
+    let inverse = search.startsWith('!');
+    if (inverse) search = search.substr(1);
+    search = search.replace(/\s+/g, '').toLowerCase();
+    
+    this.setState({
+
+      feedbacks: data.filter(f => {
+        if (f.author.replace(/\s+/g, '').toLowerCase().includes(search)) return !inverse;
+        if (f.body.replace(/\s+/g, '').toLowerCase().includes(search)) return !inverse;
+        if (f.title.replace(/\s+/g, '').toLowerCase().includes(search)) return !inverse;
+        return inverse;
+      })
+      
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-search">
+          <form onSubmit={(e) => { this.filter(e.target.elements.search.value); e.preventDefault()}}>
+            <input type="text" name="search" className="App-search-input" placeholder="Try 'nice', 'design', '!robot'..." />
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+        <div className="App-feedbacks">
+          {this.state.feedbacks.map((feedback) => <Feedback key={feedback._id} author={feedback.author} body={feedback.body} createdAt={new Date(feedback.createdAt)} profilePicture={feedback.profilePicture} stars={feedback.stars} title={feedback.title} />)}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/feedback/Feedback.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import "./Feedback.css";
+import blackStar from "./star/black.png";
+import yellowStar from "./star/yellow.png";
+
+/** @param {{ profilePicture: string, author: string, title: string, body: string, createdAt: Date, stars: number }} props */
+export default class Feedback extends Component {
+    render() {
+        return <div className="⭐">
+            <div className="⭐-header">
+                <div className="⭐-author">
+                    <img className="⭐-author-picture" alt="Author" src={this.props.profilePicture} />
+                    <h3 className="⭐-author-name">{this.props.author}</h3>
+                </div>
+
+                <div className="⭐-title">
+                    <h1 className="⭐-title-content">{this.props.title}</h1>
+                    <div className="⭐-stars">{
+                        new Array(5).fill(null).map((_, i) =>
+                            <img key={"star-" + i} className="⭐-stars-star" src={i < this.props.stars ? yellowStar : blackStar} alt="star"></img>,
+                        )
+                    }</div>
+                </div>
+            </div>
+            <p className="⭐-body">{this.props.body}</p>
+            <p className="⭐-createdAt">{this.props.createdAt.toLocaleDateString()}</p>
+        </div>
+    }
+}
